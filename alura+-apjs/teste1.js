d1e14c4cfd95ee0c2d332da94e265726b3d0b15e
leonardoMassuia <132453904+leonardo7406@users.noreply.github.com>
2023-06-13 00:12:27 -0300

    Add files via upload
    primeiros exemplos

---
+++ alura+-apjs/teste1.js
@@ -0,0 +1,7 @@
+const number1 = 23;
+const number2 = 26;
+const number3 = 6 ;
+
+const operaçao = number1 * number3;
+
+console.log (operaçao);
---
+++ alura+-apjs/teste2.js
@@ -0,0 +1,15 @@
+const string = "olá meu nome é:";
+const nome1 = " Leonardo";
+const nome2 = " mariana";
+
+const resposta = string + nome2;
+
+console.log(resposta);
+// const string = "Leonardo"
+// const string1 = "leonardo"
+
+// const padroizar = string.toLowerCase();
+
+// const comparar = padroizar === string1
+
+//  console.log(comparar)
